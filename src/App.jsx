dd65ec4d43a3aa9a4a872b4c457f213f9f1830fe
EA - Guilherme Pereira Da Rocha <pereira_rocha01@educadventista.org>
2024-04-01 14:25:13 -0300

    to arrumando

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
-
-
-
-
-
-import Luxury from "./assets/components/Luxury"
-import Sedans from "./assets/components/Sedans"
-import Suvs from "./assets/components/Suvs"
+import Luxury from './components/Luxury'
+import Sedans from './components/Sedans'
+import Suvs from './components/Suvs'
 
 export default function App() {
   return (
-    <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex jusify-center iems-center" >
-        <section className="lg:flex max-w-[920px]">
-
+    <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex jusify-center items-center" >
+        <div className="lg:flex max-w-[920px]">
           <Sedans/>
           <Suvs/>
-          <Luxury/>
-
-
-          
-
-          
-          
-        </section>
+          <Luxury/>  
+        </div>
     </main>
   )
 }
--- src/components/Sedans.jsx
+++ src/components/Sedans.jsx
@@ -5,12 +5,12 @@ export default function Sedans() {
 
     return (
 
-        <div className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
+        <section className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
             <img src={iconSedans} alt="" className="mb-7"/>
             <h2 className="font-shoulders uppercase mb-7 text-4xl font-bold text-offwhitebg">Sedans</h2>
             <p className="font-lexend mb-7 text-offwhite lg:mb-20">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
             <a href="https://www.webmotors.com.br/suvs" target="_blank" className="font-lexend bg-offwhitebg px-10 py-3 block w-fit rounded-3xl text-orange border-2 border-offwhitebg hover:bg-orange">Ver Mais</a>
-          </div>
+          </section>
 
     )
 
